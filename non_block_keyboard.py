b8ff3d38f6fa740a07991c43175ccacf6446f077
wantongtang <wantongtang@foxmail.com>
2017-12-20 17:36:12 +0800

    non_block_keyboard

--- non_block_keyboard.py
+++ non_block_keyboard.py
@@ -3,29 +3,30 @@ import time
 import sys, termios, tty, os
 
 
-kbdInput = ''
-finished = True
+if 1:
 
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-def kbdListener():
-    kbdInput = getch()
+    kbdInput = ''
     finished = True
 
-k=1
-while True:
-    if finished:
-        finished = False
-        listener = threading.Thread(target=kbdListener)
-        listener.start()
-    print kbdInput
-    time.sleep(0.2)
+    def getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    def kbdListener():
+        kbdInput = getch()
+        finished = True
+
+    while 1:
+        if finished:
+            finished = False
+            listener = threading.Thread(target=kbdListener)
+            listener.start()
+        time.sleep(0.2)
+        print kbdInput      
